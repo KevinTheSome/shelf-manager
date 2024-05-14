7d11a228ad75416a1b6cdcba8f7f422be3ada05c
Kevins Markuss Kanalis <KevinsKanalis@gmail.com>
2024-05-14 14:37:47 +0300

    Fixed and made layout

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,15 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\AuthController;
 
-Route::get('/',[AuthController::class, 'login'])->name('login');
-Route::get('/',[AuthController::class, 'register'])->name('register');
-Route::get('/',[AuthController::class, 'logout'])->name('logout');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/login',[AuthController::class, 'login'])->name('login');
+    Route::get('/register',[AuthController::class, 'register'])->name('register');
+});
+ 
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/logout',[AuthController::class, 'logout'])->name('logout');
+});
