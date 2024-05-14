9d41370b7ec285344dc4ece841dcfcf0bdc38f4c
Kevins Markuss Kanalis <KevinsKanalis@gmail.com>
2024-05-14 14:15:14 +0300

    Made auth conntroller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        return view('login');
+    }
+
+    public function register()
+    {
+        return view('register');
+    }
+
+    public function loginUser(Request $request)
+    {
+        
+    }
+
+    public function registerUser(Request $request)
+    {
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[AuthController::class, 'login'])->name('login');
+Route::get('/',[AuthController::class, 'register'])->name('register');
+Route::get('/',[AuthController::class, 'logout'])->name('logout');
+
+
