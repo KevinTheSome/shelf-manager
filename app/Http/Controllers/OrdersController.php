ac33a969d35c5c1df310e8ce6fc1204d4d03e6bc
Kevins Markuss Kanalis <KevinsKanalis@gmail.com>
2024-05-21 10:43:06 +0300

    Orders Controller and Model made

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -3,11 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Order;
 
 class OrdersController extends Controller
 {
     public function index()
     {
         return view('orders');
+    }
+
+    public function create()
+    {
+        return view('orders.create');
+    }
+
+    public function createOrder(Request $request)
+    {
+        $request->validate([
+            'orderDate' => 'required|date',
+            'receiverDate' => 'required|date',
+            'status' => 'required',
+            'amount' => 'required',
+            'user_id' => 'required',
+            'product_id' => 'required',
+        ]);
+
+        $order = new Order();
+        $order->order_date = $request->orderDate;
+        $order->receiver_date = $request->receiverDate;
+        $order->status = $request->status;
+        $order->amount = $request->amount;
+        $order->user_id = $request->user_id;
+        $order->product_id = $request->product_id;
+        $order->save();
     }
 }
