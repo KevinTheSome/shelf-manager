b4fe08b43b3ce6e8b323653b5cbd4a8c05bc229d
Kevins Markuss Kanalis <KevinsKanalis@gmail.com>
2024-05-21 12:38:04 +0300

    added Gerdas Home/Walcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,23 @@
-@include('components.navbar')
+<x-layout title="Shelf-Manager">
 
-<x-layout title="welcome">
-    <h1 class="text-center font-bold text-3xl">Welcome</h1>
+
+    <h1 class="text-center font-bold text-4xl bg-gray-200"><br>Welcome To Shelf-Manager</h1>
+
+
+    <section class="bg-gray-200 dark:bg-gray-900">
+  <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+          <h1 class="text-center text-xl font-bold leading-tight tracking-tight text-grey-900 md:text-4xl dark:text-white">Sign Up</h1>
+              <h2 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                  Want to register?
+                  <a href="/register" class="p-4 text-gray-500 hover:text-blue-500 transition">Register</a>
+              </h2>
+              <h2 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                  Already have an account?
+                  <a href="/login" class="p-4 text-gray-500 hover:text-blue-500 transition">Log In</a>
+              </h2>
+
+</div></div></div></section>
+
 </x-layout>
