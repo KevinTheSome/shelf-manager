5b4eb22657ddecec66ce1fafac286b669e7f99da
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-05-21 13:19:18 +0300

    renamed product to productcontroller

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\OrdersController;
 use App\Http\Controllers\ReportController;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\Product;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -26,13 +26,13 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/orders',[OrdersController::class, 'index'])->name('orders');
     Route::get('/report',[ReportController::class, 'index'])->name('report');
     //7 restul aciones for products
-    Route::get('/product/index', [Product::class, 'index']);
-    Route::get('/product/{id}', [Product::class, 'show']);
-    Route::post('/product/create', [Product::class, 'create']);
-    Route::get('/product/new', [Product::class, 'new']);
-    Route::get('/product/{id}/edit', [Product::class, 'edit']);
-    Route::put('/product/{id]', [Product::class, 'update']);
-    Route::delete('/product/{id}', [Product::class, 'destroy']);
+    Route::get('/product/index', [ProductController::class, 'index']);
+    Route::get('/product/{id}', [ProductController::class, 'show']);
+    Route::get('/product/create', [ProductController::class, 'create']);
+    Route::get('/product/new', [ProductController::class, 'new']);
+    Route::get('/product/{id}/edit', [ProductController::class, 'edit']);
+    Route::put('/product/{id]', [ProductController::class, 'update']);
+    Route::delete('/product/{id}', [ProductController::class, 'destroy']);
 
     Route::get('/orders/new', [OrdersController::class, 'new']);
     Route::post('/orders/create', [OrdersController::class, 'create']);
