128fb77322cfe38e8a8c4ed707d622ebc4ec060c
rudoxe <ipa22.r.vikmanis@vtdt.edu.lv>
2024-05-24 11:34:53 +0300

    fixed navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="flex w-screen justify-center space-x-8">
-    <a href="/product" class="p-4 text-gray-500 hover:text-blue-500 transition">Products</a>
+    <a href="/products/index" class="p-4 text-gray-500 hover:text-blue-500 transition">Products</a>
     <a href="/orders" class="p-4 text-gray-500 hover:text-blue-500 transition">Orders</a>
     <a href="/report" class="p-4 text-gray-500 hover:text-blue-500 transition">Report</a>
 </nav>
