43471cbf7e175f6e7f07cdfe13b70dd6b947cc6b
OskarsEngelss <126758362+OskarsEngelss@users.noreply.github.com>
2024-05-27 16:15:12 +0300

    product list design

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,9 +1,9 @@
 <x-layout title="dashboard">
     <x-navbar></x-navbar>
     <section class="container mx-auto px-4 py-8">
-        <h1 class="text-3xl font-bold text-center mb-8">Products List</h1>
+        <h1 class="text-3xl font-bold text-center mb-8 text-yellow-900">Products List</h1>
         <div class="text-center mb-4">
-        <a href="/products/create" class="inline-block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">Create New Product</a>
+        <a href="/products/create" class="inline-block bg-yellow-500 text-white py-2 px-4 rounded hover:bg-yellow-900">Create a New Product</a>
         </div>
         @if (session('success'))
             <div class="bg-green-500 text-white p-4 rounded mb-4">
@@ -14,22 +14,22 @@
             <table class="min-w-full leading-normal">
                 <thead>
                     <tr>
-                        <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">ID</th>
-                        <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">Name</th>
-                        <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">Description</th>
-                        <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">Amount</th>
-                        <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">Actions</th>
+                        <th class="bg-yellow-600 px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-yellow-900 uppercase tracking-wider">ID</th>
+                        <th class="bg-yellow-600 px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-yellow-900 uppercase tracking-wider">Name</th>
+                        <th class="bg-yellow-600 px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-yellow-900 uppercase tracking-wider">Description</th>
+                        <th class="bg-yellow-600 px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-yellow-900 uppercase tracking-wider">Amount</th>
+                        <th class="bg-yellow-600 px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-yellow-900 uppercase tracking-wider">Actions</th>
                     </tr>
                 </thead>
                 <tbody>
                 @foreach($products as $product)
     <tr>
-        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">{{ $product->id }}</td>
-        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">{{ $product->name }}</td>
-        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">{{ $product->description }}</td>
-        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">{{ $product->amount }}</td>
-        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-            <a href="{{ url('products/' . $product->id . '/edit') }}" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+        <td class="bg-yellow-100 px-5 py-5 border-b border-yellow-400 bg-white text-sm">{{ $product->id }}</td>
+        <td class="bg-yellow-100 px-5 py-5 border-b border-yellow-400 bg-white text-sm">{{ $product->name }}</td>
+        <td class="bg-yellow-100 px-5 py-5 border-b border-yellow-400 bg-white text-sm">{{ $product->description }}</td>
+        <td class="bg-yellow-100 px-5 py-5 border-b border-yellow-400 bg-white text-sm">{{ $product->amount }}</td>
+        <td class="bg-yellow-100 px-5 py-5 border-b border-yellow-400 bg-white text-sm">
+            <a href="{{ url('products/' . $product->id . '/edit') }}" class="bg-yellow-100 text-indigo-600 hover:text-indigo-900">Edit</a>
             <form action="{{ url('products/' . $product->id) }}" method="POST" class="inline-block">
                 @csrf
                 @method('DELETE')
