5c0a74955f95694f11f124a60ce2c31b2481ce5c
OskarsEngelss <126758362+OskarsEngelss@users.noreply.github.com>
2024-05-28 10:39:31 +0300

    register and login design

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,20 +1,20 @@
 <x-layout title="login">
-<section class="bg-gray-50 dark:bg-gray-900">
+<section class="bg-yellow-100">
   <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+      <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 bg-yellow-600 border-yellow-900">
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              <h1 class="text-xl font-bold leading-tight tracking-tight text-yellow-900 md:text-2xl ">
                   Sign in to your account
               </h1>
               <form method="POST" action="/loginUser" class="space-y-4 md:space-y-6"> 
                 @csrf
                   <div>
-                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
+                      <label for="email" class="block mb-2 text-sm font-medium text-white ">Email</label>
+                      <input type="email" name="email" id="email" class="bg-yellow-100 border border-yellow-600 text-yellow-900 sm:text-sm rounded-lg focus:ring-yelllow-900 focus:border-yellow-900 block w-full p-2.5 " placeholder="name@company.com" required="">
                   </div>
                   <div>
-                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                      <label for="password" class="block mb-2 text-sm font-medium text-white">Password</label>
+                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-yellow-100 border border-yellow-600 text-yellow-900 sm:text-sm rounded-lg focus:ring-yellow-900 focus:border-yellow-900 block w-full p-2.5 " required="">
                   </div>
                   @if ($errors->any())
                   <div class="alert alert-danger">
@@ -25,9 +25,10 @@
                       </ul>
                   </div>
               @endif
-                  <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                  <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Don’t have an account yet? <a href="/register" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                  <button type="submit" class="w-full text-white bg-yellow-500 hover:bg-yellow-900 focus:ring-4 focus:outline-none focus:ring-yellow-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                    Sign in</button>
+                  <p class="text-sm font-light text-white ">
+                      Don’t have an account yet? <a href="/register" class="font-medium text-yellow-900 hover:underline">Sign up</a>
                   </p>
               </form>
           </div>
