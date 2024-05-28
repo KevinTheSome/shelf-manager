c012da7fb065a806645bf678b57369b8a7c17add
OskarsEngelss <126758362+OskarsEngelss@users.noreply.github.com>
2024-05-28 09:23:50 +0300

    elcome blade style changes

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="flex w-screen justify-center space-x-8 bg-yellow-100">
-    <a href="/dashboard" class="p-4 text-yellow-900 hover:text-yellow-500 transition">Dashboard</a>
-    <a href="/products/index" class="p-4 text-yellow-900 hover:text-yellow-500 transition">Products</a>
-    <a href="/orders" class="p-4 text-yellow-900 hover:text-yellow-500 transition">Orders</a>
-    <a href="/report" class="p-4 text-yellow-900 hover:text-yellow-500 transition">Report</a>
-    <a href="/shelf/index" class="p-4 text-yellow-900 hover:text-yellow-500 transition">Shelves</a>
+    <a href="/dashboard" class="p-4 text-yellow-900 hover:text-yellow-400 transition">Dashboard</a>
+    <a href="/products/index" class="p-4 text-yellow-900 hover:text-yellow-400 transition">Products</a>
+    <a href="/orders" class="p-4 text-yellow-900 hover:text-yellow-400 transition">Orders</a>
+    <a href="/report" class="p-4 text-yellow-900 hover:text-yellow-400 transition">Report</a>
+    <a href="/shelf/index" class="p-4 text-yellow-900 hover:text-yellow-400 transition">Shelves</a>
 </nav>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,5 @@
 <x-layout title="dashboard">
-<a href="/logout" class="items-right flex flex-col text-right p-4 text-gray-500 hover:text-blue-500 transition">Log Out</a>
+<a href="/logout" class="items-right flex flex-col text-right p-4 text-yellow-900 hover:text-yellow-400 transition">Log Out</a>
 <section>
         <div class="container mx-auto">
             <div class="bg-white rounded-lg shadow border bg-yellow-600 border-gray-700 mt-6">
@@ -11,42 +11,42 @@
                         
                         <div class="bg-yellow-100 p-5 rounded-lg shadow">
                             <h3 class="text-lg font-semibold mb-3">
-                            <a href="/products/index" class=" p-4 text-yellow-900 hover:text-blue-500 transition">Products</a>
+                            <a href="/products/index" class=" p-4 text-yellow-900 hover:text-yellow-400 transition">Products</a>
                             </h3>
                             <a href='products/create' class="text-yellow-700 text-yellow-400">Create a Product</a>
                         </div>
                         
                         <div class="bg-yellow-100 p-5 rounded-lg shadow">
                             <h3 class="text-lg font-semibold mb-3">
-                            <a href="/orders" class="p-4 text-yellow-900 hover:text-blue-500 transition">Orders</a>
+                            <a href="/orders" class="p-4 text-yellow-900 hover:text-yellow-400 transition">Orders</a>
                             </h3>
                             <a href='/orders/create' class="text-yellow-700 text-yellow-400">Create an Order</a>
                         </div>
                         
                         <div class="bg-yellow-100 p-5 rounded-lg shadow">
                             <h3 class="text-lg font-semibold mb-3">
-                            <a href="/report" class="p-4 text-yellow-900 hover:text-blue-500 transition">Reports</a>
+                            <a href="/report" class="p-4 text-yellow-900 hover:text-yellow-400 transition">Reports</a>
                             </h3>
                             
                         </div>
 
                         <div class="bg-yellow-100 p-5 rounded-lg shadow">
                             <h3 class="text-lg font-semibold mb-3">
-                            <a href="/admin" class="p-4 text-yellow-900 hover:text-blue-500 transition">Admin</a>
+                            <a href="/admin" class="p-4 text-yellow-900 hover:text-yellow-400 transition">Admin</a>
                             </h3>
                             <a href='/admin/users' class="text-yellow-700 text-yellow-400">Manage Users</a>
                         </div>
 
                         <div class="bg-yellow-100 p-5 rounded-lg shadow">
                             <h3 class="text-lg font-semibold mb-3">
-                            <a href="/shelf/index" class="p-4 text-yellow-900 hover:text-blue-500 transition">Shelves</a>
+                            <a href="/shelf/index" class="p-4 text-yellow-900 hover:text-yellow-400 transition">Shelves</a>
                             </h3>
                             <a href='/shelf/create' class="text-yellow-700 text-yellow-400">Add a Shelves</a>
                         </div>
 
                         <div class="bg-yellow-100 p-5 rounded-lg shadow">
                             <h3 class="text-lg font-semibold mb-3">
-                            <a href="/admin" class="p-4 text-yellow-900 hover:text-blue-500 transition">Shelves Storage</a>
+                            <a href="/admin" class="p-4 text-yellow-900 hover:text-yellow-400 transition">Shelves Storage</a>
                             </h3>
                             <a href='/admin/users' class="text-yellow-700 text-yellow-400">Manage Products in what shelf</a>
                         </div>
