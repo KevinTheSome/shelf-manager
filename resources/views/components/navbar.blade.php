22a79517277776fe9fd8444e9161dc3a90270c74
Kevins Markuss Kanalis <KevinsKanalis@gmail.com>
2024-05-28 08:40:20 +0300

    and nav

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,5 @@
 <nav class="flex w-screen justify-center space-x-8 bg-yellow-100">
+    <a href="/dashboard" class="p-4 text-yellow-900 hover:text-yellow-500 transition">Dashboard</a>
     <a href="/products/index" class="p-4 text-yellow-900 hover:text-yellow-500 transition">Products</a>
     <a href="/orders" class="p-4 text-yellow-900 hover:text-yellow-500 transition">Orders</a>
     <a href="/report" class="p-4 text-yellow-900 hover:text-yellow-500 transition">Report</a>
