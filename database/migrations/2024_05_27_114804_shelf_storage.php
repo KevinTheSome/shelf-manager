9449456b3fe7acab2faadea0bba76fa16489b326
Kevins Markuss Kanalis <KevinsKanalis@gmail.com>
2024-05-29 09:23:10 +0300

    Fixed migratione

--- database/migrations/2024_05_27_114804_shelf_storage.php
+++ database/migrations/2024_05_27_114804_shelf_storage.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('product_id')->references('id')->on('products')->onDelete('cascade');
             $table->foreignId('shelf_id')->references('id')->on('shelves')->onDelete('cascade');
+            $table->timestamps();
         });
     }
 
